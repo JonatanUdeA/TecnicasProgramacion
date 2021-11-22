807e2556130c1f8d521c74b2e04b83f836ae649c
JonatanR <jonatanjr-2017@outlook.es>
2021-11-22 15:26:35 -0500

    Hola Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hola Intellij IDEA ");
+
+        System.out.println("Eres demasiado lento para trabajar no no que rabia");
+    }
+}
