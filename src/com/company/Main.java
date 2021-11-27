1ffa9bec5a7df9848b779b8f9c60d199e63d7da6
JonatanR <jonatanjr-2017@outlook.es>
2021-11-27 13:58:22 -0500

    Instancia de un objeto de la clase Scanner ('input')

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,14 @@ public class Main
     public static void main(String[] args)
     {
        Scanner input = new Scanner(System.in);
+
+       int n;
+
+       do{
+           System.out.println("Ingresa un numero positivo");
+           n = input.nextInt();
+       }while(n<0);
+
+        System.out.println("Excelente !!, Tu numero Es "+n);
     }
 }
