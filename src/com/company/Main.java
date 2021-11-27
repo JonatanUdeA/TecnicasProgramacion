d637cdd427ac6cf2e0b9c61ff795700eccaafffe
JonatanR <jonatanjr-2017@outlook.es>
2021-11-27 11:36:03 -0500

    Empezando desde cero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Hola Intellij IDEA ");
 
-        System.out.println("Eres demasiado lento para trabajar no no que rabia");
     }
 }
