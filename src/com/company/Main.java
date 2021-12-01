1b19c9fcfa4035e65fc23e05635831db12bb22a4
JonatanR <jonatanjr-2017@outlook.es>
2021-11-30 20:55:07 -0500

    primer ejercicio de la clase.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
-import java.util.*;
-public class Main
-{
 
-    public static void main(String[] args)
-    {
-       Scanner input = new Scanner(System.in);
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, resultado;
+
+        num1 = 2;
+        num2 = 5;
+
+        resultado = num1 + num2;
+
+        System.out.println("El resultado es " + resultado);
+
     }
 }
